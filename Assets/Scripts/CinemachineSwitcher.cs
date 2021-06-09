e52f5927fa12a0c55997bf12596015dc7ced3814
Slaculeanu <laculeanu.stefan@gmail.com>
2021-06-09 23:13:51 +0300

    Cinemachine added (lerp between 2 cameras)

---
+++ Assets/Scripts/CinemachineSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CinemachineSwitcher : MonoBehaviour {
+    public Animator ralphAnimator;
+    public Animator astraAnimator;
+
+    private Animator animator;
+
+    private bool isAstraCamera = true;
+    public bool canSwitch = false;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void SwitchState() {
+        if (isAstraCamera) {
+            animator.Play("Ralph camera");
+            ralphAnimator.SetBool("ShutDown", false);
+        } else {
+            ralphAnimator.SetBool("ShutDown", true);
+            animator.Play("Astra camera");
+        }
+        isAstraCamera = !isAstraCamera;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("space") && canSwitch) {
+            SwitchState();
+        }
+    }
+}
