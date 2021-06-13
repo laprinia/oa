006738403876f25b1f898d9c7e386a7358773ab9
JUNO\Lavinia <lavinialoghin7@gmail.com>
2021-06-14 02:15:23 +0300

    no message

---
+++ Assets/DisableAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAnimation : MonoBehaviour
+{ public Animator canvasAnimator;
+   private void Update()
+   {
+
+      if (Input.GetKey(KeyCode.Space))
+      {
+         canvasAnimator.SetTrigger("disappear");
+         Destroy(gameObject);
+      }
+      
+   }
+}
