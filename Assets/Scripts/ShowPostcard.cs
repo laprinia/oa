6d904bf2bbd8b19060b8b6bec8b9d0f70b89273d
JUNO\Lavinia <lavinialoghin7@gmail.com>
2021-06-13 21:00:44 +0300

    added end game canvas

---
+++ Assets/Scripts/ShowPostcard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPostcard : MonoBehaviour
+{
+   [SerializeField] private Animator _animator;
+   public void ShowPost()
+   {
+      _animator.SetTrigger("appear");
+   }
+   public void HidePost()
+   {
+      _animator.SetTrigger("diasppear");
+   }
+}
