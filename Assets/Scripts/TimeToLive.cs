dbd4d673193ae18fbd455e344ba946d59abab13c
JUNO\Lavinia <lavinialoghin7@gmail.com>
2021-06-13 16:24:18 +0300

    added UI element for ralph's time to live

--- Assets/Scripts/TimeToLive.cs
+++ Assets/Scripts/TimeToLive.cs
@@ -2,12 +2,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeToLive : MonoBehaviour
 {
     public bool ralphTimeToLiveExpired;
     public float timeToWait;
+    public Image ringBar;
+    public Color startColor, endColor;
+    private float lerpSpeed;
+    private float totalTimeToWait;
     
+
+    private void Start() => totalTimeToWait = timeToWait;
+
+    private void Update()
+    {
+        lerpSpeed = 3f * Time.deltaTime;
+        ColorChanger();
+        HealthBarFiller();
+    }
+    void HealthBarFiller()
+    {
+       
+        ringBar.fillAmount = Mathf.Lerp(ringBar.fillAmount, (timeToWait / totalTimeToWait), lerpSpeed);
+
+    }
+
+    void ColorChanger()
+    {
+        Color healthColor = Color.Lerp(endColor, startColor, (timeToWait / totalTimeToWait));
+        ringBar.color = healthColor;
+    }
+
     public void ResetTimer()
     {
         ralphTimeToLiveExpired = false;
